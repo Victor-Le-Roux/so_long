7073db83aa616d72523238943d1fccd318222af0
victor <victorleroux92@gmail.com>
2023-11-30 14:39:35 +0100

    marios_bros_afficher

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all	:	
-	gcc main.c -I ./MLX42/include -L ./MLX42/build -lmlx42 -lglfw -lm -ldl -lpthread -o my_program
+	gcc main.c -I ./MLX42/include -L ./MLX42/build -lmlx42 -lglfw -lm -ldl -lpthread -o my_program | ./my_program
--- main.c
+++ main.c
@@ -1,17 +1,44 @@
 #include <MLX42/MLX42.h>
+#include <stdlib.h>
 
-int main(void)
-{
+int main(void) {
     mlx_t* mlx;
+    mlx_texture_t* img;
+    mlx_image_t* image;
 
+    // Initialize MLX42
     mlx = mlx_init(800, 600, "My MLX42 Window", false);
     if (!mlx)
-        return (1);
+        return EXIT_FAILURE;
 
+    // Load an image
+    img = mlx_load_png("./mario_bros_normal.png"); // Replace with your image path
+    if (!img) {
+        mlx_terminate(mlx);
+        return EXIT_FAILURE;
+    }
 
+    // Convert texture to image
+    image = mlx_texture_to_image(mlx, img);
+    if (!image) {
+        mlx_delete_texture(img);
+        mlx_terminate(mlx);
+        return EXIT_FAILURE;
+    }
 
+    // Display the image in the window
+    if (mlx_image_to_window(mlx, image, 0, 0) == -1) {
+        mlx_delete_texture(img);
+        mlx_terminate(mlx);
+        return EXIT_FAILURE;
+    }
+
+    // Main loop to keep the window open
     mlx_loop(mlx);
-    mlx_terminate(mlx);
-    return (0);
-}
 
+    // Clean up
+    mlx_delete_texture(img);
+    mlx_terminate(mlx);
+
+    return EXIT_SUCCESS;
+}
