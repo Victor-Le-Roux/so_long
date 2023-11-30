a48e96c792299d304081a9ad03d56a78ee3ff354
victor <victorleroux92@gmail.com>
2023-11-30 12:45:17 +0100

    fenetre cree

---
+++ Makefile
@@ -0,0 +1,2 @@
+all	:	
+	gcc main.c -I ./MLX42/include -L ./MLX42/build -lmlx42 -lglfw -lm -ldl -lpthread -o my_program
--- main.c
+++ main.c
@@ -1,26 +1,17 @@
-#include "minilibx-linux/mlx.h"
-#include <stdlib.h>
+#include <MLX42/MLX42.h>
 
 int main(void)
 {
-    void *mlx_ptr;
-    void *win_ptr;
+    mlx_t* mlx;
 
-    // Initialiser la MiniLibX
-    mlx_ptr = mlx_init();
-    if (mlx_ptr == NULL)
-        return (1);  // Échec de l'initialisation
+    mlx = mlx_init(800, 600, "My MLX42 Window", false);
+    if (!mlx)
+        return (1);
 
-    // Créer une fenêtre
-    win_ptr = mlx_new_window(mlx_ptr, 800, 600, "Ma fenêtre MiniLibX");
-    if (win_ptr == NULL)
-    {
-        mlx_destroy_display(mlx_ptr);
-        free(mlx_ptr);
-        return (1);  // Échec de la création de la fenêtre
-    }
 
-    // Lancer la boucle d'événements
-    mlx_loop(mlx_ptr);
+
+    mlx_loop(mlx);
+    mlx_terminate(mlx);
     return (0);
 }
+
